860c2924c68ab3aa5bc53a1ff3560a58a1cbaa5a
ValenBorras <106563001+ValenBorras@users.noreply.github.com>
2023-02-01 16:50:28 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { NavBar } from './components/NavBar';
 const App = () => {
 
   const greeting = 'Buenas, bienvenid@ a Game Stop! *nombre totalmente original* ;)'
+  
   return (
     <body className=' bg-gradient-to-br from-slate-900 to-indigo-900 h-screen w-screen font-sanz'>
       <NavBar/>
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const ItemListContainer = ({greeting}) => {
   return (
-    <>
+    
     <h1 className=' text-slate-200 font-semibold text-2xl mt-20 text-center bg-transparent'>{greeting}</h1>
-    </>
+    
   )
 }
